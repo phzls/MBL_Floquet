4d033c1fec3949fa6a094ec7865b057747ec2578
Liangsheng <liangshe@princeton.edu>
2014-12-19 18:21:13 -0500

    Add data for Evol_construction

--- Floquet/evol_class.h
+++ Floquet/evol_class.h
@@ -1,6 +1,18 @@
 #include <cmath>
+#include <string>
 
 using namespace std;
+
+/*
+A pure base structure of parameters which gives data used for the construction of time evolution
+operator.
+*/
+template <class T>
+struct ModelConstructData
+{
+	T data_set; // The data set that will be used
+	virtual void Initialize() = 0; // Initialize the data
+};
 
 /*
 A base class defines the evolution of a quantum system. The size of the system needs to
@@ -25,7 +37,8 @@ class EvolMatrix
 		EvolMatrix(int size, int local_dim):
 			size_(size), local_dim_(local_dim), dim_(int(pow(double(local_dim), double(size)))){}
 
-		virtual void Evol_Construct() = 0; // Constructing time evolution matrix
+		// Constructing time evolution matrix
+		virtual void Evol_Construct(const ModelConstructData<class T>&) = 0; 
 		
 		// Diagnolize time evolution matrix with eigenvectors kept
 		virtual void Evol_Diag() = 0;
@@ -33,6 +46,9 @@ class EvolMatrix
 		// Diagnolize time evolution matrix, user can determine whether eigenvectors are kept
 		// False is not kept; True is kept
 		virtual void Evol_Diag(bool keep) = 0;
+
+		// Return the string format of representation string stream 
+		virtual string Repr() const = 0; 
 
 		virtual ~EvolMatrix();
 };
