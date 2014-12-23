2c391e5355b326cf33e2402e06e9e6d159c9e117
Liangsheng <liangshe@princeton.edu>
2014-12-23 18:56:36 -0500

    Let construction controls which part to be outputted

--- Floquet/level_stats.h
+++ Floquet/level_stats.h
@@ -22,10 +22,16 @@ This class outputs level statistics for vanilla Floquet time evolution operators
 class VanillaFloLevel: public ResultsOutput< EvolMatrix< ComplexEigenSolver<MatrixXcd> > >
 {
 	private:
-		vector<double> level_;
-		stringstream filename_;
+		vector<double> level_; // The vector that holds all the level spacings
+		stringstream filename_; // filename
+		const bool spacing_; // If it is true, level spacing will be outputted
+		const bool mean_; // If it is true, the mean of level spacings will be outputted
+		const bool mean_square_; // If it is true, the mean of square will be outputted
 
 	public:
+		VanillaFloLevel(bool spacing, bool mean, bool mean_square):
+			spacing_(spacing), mean_(mean), mean_square_(mean_square) {};
+			
 		void Data_Process(const EvolMatrix< ComplexEigenSolver<MatrixXcd> >& );
 		void Data_Output();
 
