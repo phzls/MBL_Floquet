64fe08cebc59eb09e39eef7508796ff439193cf6
Liangsheng <liangshe@princeton.edu>
2014-12-25 13:01:48 -0500

    Add more functions

--- Floquet/level_stats.h
+++ Floquet/level_stats.h
@@ -3,6 +3,7 @@
 
 #include <sstream>
 #include <vector>
+#include <utility>
 #include <Eigen/Dense>
 #include <Eigen/Eigenvalues>
 #include "results.h"
@@ -19,21 +20,55 @@ time evolution operator
 /*
 This class outputs level statistics for vanilla Floquet time evolution operators
 */
-class VanillaFloLevel: public ResultsOutput< EvolMatrix< ComplexEigenSolver<MatrixXcd> > >
+class VanillaFloLevel: public ResultsOutput< EvolMatrix< ComplexEigenSolver<MatrixXcd> >*, 
+pair<vector<double>, vector<double> >
 {
 	private:
 		vector<double> level_; // The vector that holds all the level spacings
-		stringstream filename_; // filename
-		const bool spacing_; // If it is true, level spacing will be outputted
-		const bool mean_; // If it is true, the mean of level spacings will be outputted
-		const bool mean_square_; // If it is true, the mean of square will be outputted
+		
+		double mean_; // The mean of level spacing
+		double mean_sd_; // Standard deviation of the mean
+
+		double square_mean_; // The square mean of level spacing
+		double square_mean_sd_; // The standard deviation of square mean
+
+		stringstream base_filename_; // base filename stringstream
+
+		const bool level_out_; // If it is true, level spacing will be outputted
+		const bool mean_out_; // If it is true, the mean of level spacings will be outputted
+		const bool mean_square_out_; // If it is true, the mean of square will be outputted
+		const bool redirect_; // If true, redirect the output of mean and square mean
+
+		// Process data from single realization. Return a pair of doubles. The first is the
+		// mean, and the second is the square mean
+		pair<double, double> Single_Data_Process_
+		(const EvolMatrix< ComplexEigenSolver<MatrixXcd> >* const);
+
+		bool init_; // Check whether the object has processed one set of data
 
 	public:
-		VanillaFloLevel(bool spacing, bool mean, bool mean_square):
-			spacing_(spacing), mean_(mean), mean_square_(mean_square) {};
+		VanillaFloLevel(bool level_out, bool mean_out, bool mean_square_out):
+			level_out_(level_out), mean_out_(mean_out), mean_square_out_(mean_square_out),
+			redirect_(false), mean_(0), mean_sd_(0), square_mean_(0), square_mean_sd_(0),
+			init_(false) {};
+
+		VanillaFloLevel():
+			levelfalse), meafalse), mean_square_out_(false), redirect_(true), mean_(0), 
+			mean_sd_(0), square_mean_(0), square_mean_sd_(0), init_(false) {};
 			
-		void Data_Process(const EvolMatrix< ComplexEigenSolver<MatrixXcd> >& );
-		void Data_Output();
+		void Data_Process(const vector< EvolMatrix< ComplexEigenSolver<MatrixXcd> >* >& );
+		void Data_Output(bool output) const;
+
+		/*
+		Redirect data to a pair. The first vector takes the level spacings. The second takes mean,
+		square mean and their standard deviations. The order is: mean, mean sd, square mean, square
+		mean sd.
+		*/
+		void Data_Redirect(pair<vector<double>, vector<double>&) const;
+
+		bool Empty() const;
+
+		void Reset();
 
 		~VanillaFloLevel() {};
 };
