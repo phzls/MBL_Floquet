cf5d6422a3fa9d1ce0d8c218c73da2bfbff23405
Liangsheng <liangshe@princeton.edu>
2014-12-25 13:02:38 -0500

    Add header file for comparing functions in sorting

---
+++ Floquet/sort.h
@@ -0,0 +1,10 @@
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+/**
+This is the header file for a variaty of comparison functions used in sorting functions
+**/
+
+bool Vec_Double_Sort(double i, double j) {return (i<j);}
