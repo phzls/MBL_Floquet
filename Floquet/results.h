e81877377713093c916cb739e6c063f32a71b3f2
Liangsheng <liangshe@princeton.edu>
2014-12-29 10:52:39 -0500

    Modify comments

--- Floquet/results.h
+++ Floquet/results.h
@@ -6,20 +6,23 @@
 using namespace std;
 
 /**
-This file defines a pure base class for processing raw data and output it to a file. The
-raw data, which may come from multiple instances of calculations, should be passed in as
-a vector. The object can also redirect the processed data to outside. The object can only
-process data once and only store one set of data. If it is used to process multiple data sets,
-then it must be reset in between, which loses all information about the previous data set.
-**/
+ ** This file defines a pure base class for processing raw data and output it to a file. The
+ ** raw data, which may come from multiple instances of calculations, should be passed in as
+ ** a vector. The object can also redirect the processed data to outside. The object can only
+ ** process data once and only store one set of data. If it is used to process multiple data
+ ** sets, then it must be reset in between, which loses all information about the previous
+ ** data set.
+ **/
 
 template <class T1, class T2>
 class ResultsOutput
 {
 	public:
-		/* The function which processes all the data. It may call different functions specific
-		to particular derived classes to hand different data, and different bools to control
-		excution of calculations. */
+		/* 
+		 * The function which processes all the data. It may call different functions
+		 * specific to particular derived classes to hand different data, and different
+		 * bools to control excution of calculations. 
+		 */
 		virtual void Data_Process(const vector<T1>& ) = 0;
 
 		// Output data to a file. The bool decides whether filenames are outputed. The
