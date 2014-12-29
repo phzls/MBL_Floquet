b35d0982fc82fb46d5c8b3587256d1f4afcb27fc
Liangsheng <liangshe@princeton.edu>
2014-12-29 10:51:24 -0500

    Add main file

---
+++ Floquet/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <ctime>
+#include "parameters.h"
+#include "mtrand.h"
+#include "task.h"
+
+using namespace std;
+
+MTRand u1rand(time(NULL));
+MTRand_closed u2rand(time(NULL));
+
+int main(){
+	AllPara parameters;
+	const bool Flo = true;
+	const bool Level = true;
+
+	parameters.generic.size = 4; // System size
+	parameters.generic.num_realizations = 400; // Number of realizations
+	parameters.generic.threads_N = 4; // Number of threads in openmp
+	parameters.generic.evec = false; // Whether compute eigenvectors
+	parameters.generic.erase = true; // Whether erase matrix after diagonization
+
+	parameters.output.width = 15; // Width for spacing in output files
+	parameters.output.filename_output = true; // Whether print out file names
+
+	parameters.floquet.J_N = 11; // Number of points of coupling strength
+	parameters.floquet.J_min = 0; // Minimum J
+	parameters.floquet.J_max = 1; // Maximum J
+	parameters.floquet.tau = 0.8; // Time step size
+
+	if (Flo){
+		if (Level){
+			flo_level(parameters);
+		}
+	}
+
+	return 0;
+}
