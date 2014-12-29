c97386b312808ef38810c38fd477f1b1b012aee9
Liangsheng <liangshe@princeton.edu>
2014-12-29 10:51:04 -0500

    Modify comments

--- Floquet/output_func.h
+++ Floquet/output_func.h
@@ -5,24 +5,35 @@
 using namespace std;
 
 /**
-This file contains some functions which are used for writing out files.
-**/
+ ** This file contains some functions which are used for writing out files.
+ **/
 
-// Given a base filename and string after it. The bool determines whether the filename 
-// is outputted
+/* 
+ * Given a base filename and string after it. The bool determines whether the filename 
+ * is outputted
+ */
 void Of_Construct(ofstream&, const stringstream&, const string&, bool);
 
-// Given a base filename and string before it. The bool determines whether the filename 
-// is outputted
+/* 
+ * Given a base filename and string before it. The bool determines whether the filename 
+ * is outputted
+ */
 void Of_Construct(ofstream&, const string&, const stringstream&, bool);
 
-// Given a base filename and strings both before and after it. The bool determines whether  
-// the filename is outputted
+/* 
+ * Given a base filename and strings both before and after it. The bool determines whether  
+ * the filename is outputted
+ */
 void Of_Construct(ofstream&, const string&, const stringstream&, const string&, bool);
 
-// Write out the vector in a line to a file. The integer gives the space between two numbers
+/* 
+ * Write out the vector in a line to a file. The integer gives the space between two numbers
+ * in the output files.
+ */
 void Write_File(ofstream&, const vector<double>&, int);
 
-// Write out the vector in a line to a file. The double is some parameter, and the
-// integer gives the space between two numbers
+/* 
+ * Write out the vector in a line to a file. The double is some parameter, and the
+ * integer gives the space between two numbers in the output files.
+ */
 void Write_File(ofstream&, double, const vector<double>&, int);
