f6ce25ea7edfedcc662341735436cd8f405a12c1
Liangsheng <liangshe@princeton.edu>
2014-12-29 11:05:46 -0500

    Add header file for constants

---
+++ Floquet/constants.h
@@ -0,0 +1,20 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <complex>
+
+using namespace std;
+
+/**
+ ** Constants used throughout files
+ **/
+
+const double Pi = 3.1415926535897932;
+
+const complex<double> Complex_zero = complex<double>(0,0);
+
+const complex<double> Complex_one = complex<double>(1,0);
+
+const complex<double> Complex_I = complex<double>(0,1);
+
+#endif
