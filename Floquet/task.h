fa5b420f994d20694b6da289eee7dc9ff82036ea
Liangsheng <liangshe@princeton.edu>
2014-12-29 11:06:58 -0500

    Add header file for tasks

---
+++ Floquet/task.h
@@ -0,0 +1,15 @@
+#ifndef TASKS_H
+#define TASKS_H
+
+#include "parameters.h"
+
+/**
+ ** This file includes all the tasks that can be called from the main function.
+ **/
+
+/*
+ * Compute level statistics for floquet system.
+ */
+void flo_level(const AllPara&);
+
+#endif
