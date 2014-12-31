22faea500659acca681d372cf50030468600daaf
Liangsheng <liangshe@princeton.edu>
2014-12-31 12:40:56 -0500

    Creat Random Rotation Floquet Model

--- Floquet/flo_evol_model.cpp
+++ Floquet/flo_evol_model.cpp
@@ -9,6 +9,10 @@ extern MTRand_closed u2rand; // points in [0,1]
 
 using namespace std;
 
+/**
+ ** Implementation of FloEvolRandom class.
+ **/
+
 /*
  * Construct the representation string and abstract type of the class.
  */
@@ -203,7 +207,201 @@ void FloEvolRandom::Evol_Z_Construct_(MatrixXcd& Uz){
 	}
 }
 
-void FloEvolRandom::Evol_Erase(){
-	evol_op_.resize(0,0);
-	constructed_ = false;
+//===============================================================================================
+
+/**
+ ** Implementation of FloEvolRandomRotation class.
+ **/
+
+/*
+ * Construct the representation string and abstract type of the class.
+ */
+void FloEvolRandomRotation::Repr_Init_(){
+	repr_ << "Random_Rotation_Floquet_L=" << size_ << ",J=" << param_.J <<",tau="
+		  << param_.tau;
+	type_ = "Random_Rotation_Floquet";
+}
+
+/*
+ * Initialize parameters of the model.
+ */
+void FloEvolRandomRotation::Evol_Para_Init(){
+	angle_.resize(size_);
+	axis_.resize(size_);
+
+	for (int i=0; i<size_; i++){
+		axis_[i].resize(3);
+	}
+
+	// We generate psi and xi randomly in [0, 2*pi), and (sin(phi))^2 randomly from 
+	// [0, 1] where phi belongs to [0, pi/2]
+	for (int i=0; i<size_; i++){
+		angle_[i] = 2*Pi * u1rand();
+
+		double z = 2 * u2rand() - 1;
+		double phi = 2 * Pi * u1rand();
+
+		if (debug_) cout << "angle: " << angle_[i] << " phi: " << phi << endl;
+
+		axis_[i][0] = sqrt(1 - z*z) * cos(phi);
+		axis_[i][1] = sqrt(1 - z*z) * sin(phi);
+		axis_[i][2] = z;
+	}
+
+	if (debug_){
+		cout<<"All random numbers:"<<endl;
+		for (int i=0; i<size_; i++){
+			cout<<i<<"  ";
+			cout << angle_[i] << " ";
+			for (int j=0; j<axis_[i].size(); j++) cout<<axis_[i][j]<<"  ";
+			cout<<endl;
+		}
+	}
+}
+
+/*
+ * Construct time evolution operator. 
+ */
+void FloEvolRandomRotation::Evol_Construct(){
+	// If the matrix has not been constructed
+	if (!constructed_){
+		evol_op_ = MatrixXcd::Zero(dim_, dim_);
+		constructed_ = true;
+	}
+
+	// Ux part of time evolution operator
+	MatrixXcd Ux = MatrixXcd::Zero(dim_, dim_);
+	Evol_Site_Construct_(Ux);
+
+	// Uz part of time evolution operator
+	MatrixXcd Uz = MatrixXcd::Zero(dim_, dim_);
+	Evol_Z_Construct_(Uz);
+
+	evol_op_ = Ux * Uz;
+
+	// In the debug mode, print out all matrices
+	if (debug_){
+		cout<<"Ux:"<<endl;
+		for (int i=0; i< Ux.rows();i++){
+			for (int j=0; j< Ux.cols();j++){
+				cout<<"("<<real(Ux(i,j))<<","<<imag(Ux(i,j))<<")  ";
+			}
+			cout<<endl;
+		}
+		cout<<endl;
+
+		cout<<"Uz:"<<endl;
+		for (int i=0; i< Uz.rows();i++){
+			for (int j=0; j< Uz.cols();j++){
+				cout<<"("<<real(Uz(i,j))<<","<<imag(Uz(i,j))<<")  ";
+			}
+			cout<<endl;
+		}
+		cout<<endl;
+
+		cout<<"Final matrix:"<<endl;
+		for (int i=0; i< evol_op_.rows();i++){
+			for (int j=0; j< evol_op_.cols();j++){
+				cout<<"("<<real(evol_op_(i,j))<<","<<imag(evol_op_(i,j))<<")  ";
+			}
+			cout<<endl;
+		}
+		cout<<endl;
+	}
+}
+
+/*
+ * Construct random Ux part.
+ */
+void FloEvolRandomRotation::Evol_Site_Construct_(MatrixXcd& Ux){
+	int current_dim = 1; // The current dimension of the matrix in the Kroneker product
+
+	// Random SU(2) matrix at each site
+	MatrixXcd U(2,2);
+
+	Ux(0,0) = Complex_one;
+
+	for (int site = 0; site < size_; site++){
+
+		// Construct U matrix at each site
+		U(0,0) = complex<double>(cos(angle_[site]), axis_[site][2]*sin(angle_[site]));
+		U(0,1) = complex<double>(sin(angle_[site])*axis_[site][1], 
+								 sin(angle_[site])*axis_[site][0]);
+		U(1,0) = -conj(U(0,1));
+		U(1,1) = conj(U(0,0));	
+
+		if (debug_){
+			cout<<"At site: "<<site<<" U:"<<endl;
+			for (int i=0; i< U.rows();i++){
+				for (int j=0; j< U.cols();j++){
+					cout<<"("<<real(U(i,j))<<","<<imag(U(i,j))<<")  ";
+				}
+				cout<<endl;
+			}
+			cout<<endl;
+		}
+
+		// Doing Kroneker Product operation with U at left
+		for (int i=0; i<current_dim;i++){ // The second block in the row
+			for (int j=0; j<current_dim; j++){
+				Ux(i, j+current_dim) = Ux(i,j) * U(0,1);
+			}
+		}
+
+		for (int i=0; i<current_dim; i++){ // The second block in the column
+			for (int j=0; j<current_dim; j++){
+				Ux(i+current_dim, j) = Ux(i,j) * U(1,0); 
+			}
+		}
+
+		for (int i=0; i<current_dim; i++){ // The second block on the diagonal
+			for (int j=0; j<current_dim; j++){
+				Ux(i+current_dim, j+current_dim) = Ux(i,j) * U(1,1);
+			}
+		}
+
+		for (int i=0; i<current_dim; i++){ // The first block
+			for (int j=0; j<current_dim; j++){
+				Ux(i,j) *= U(0,0);
+			}
+		}
+
+		current_dim *= 2;
+	}
+
+	if (current_dim < dim_){
+		cout << "Kroneker Product fails to produce the correct dimension."<<endl;
+		abort();
+	}
+}
+
+/*
+ * Construct Uz part.
+ */
+void FloEvolRandomRotation::Evol_Z_Construct_(MatrixXcd& Uz){
+	/*
+	 * We are working in S_z basis, and 0 means down while 1 means up when each state is 
+	 * encoded in a number. The total state will be encoded by a single number between 0
+	 * and dim_, whose binary representation, from left to right, gives each spin
+	 * orientation at each site, from left to right. When computing the matrix element, 
+	 * -1 is for down spin and 1 is for up spin.
+	 */
+
+	for (int i=0; i<dim_; i++){
+		int spin = i;
+		int current_spin = 2 * (spin % 2) - 1;
+		spin /=  2;
+		int next_spin;
+		double H = 0; // The logarithm of the matrix element
+
+		for (int j=0; j<size_ - 1; j++){
+			next_spin = 2 * (spin % 2) - 1;
+			spin /= 2;
+
+			H += param_.J * next_spin * current_spin;
+
+			current_spin = next_spin;
+		}
+		Uz(i,i) = exp(-Complex_I * complex<double>(H,0) );
+	}
 }
--- Floquet/flo_evol_model.h
+++ Floquet/flo_evol_model.h
@@ -32,15 +32,13 @@ class FloEvolRandom : public FloEvolVanilla
 		// The four random angles used in sampling random SU(2) matrices
 		vector<vector<double> > su2_angle_; 
 
-		stringstream repr_; // Representation string stream of the model
-		string type_; // Type string of the model
-		void Repr_Init_(); // Initialize the representation string stream as well as type
-
 		// Construct Ux part in time evolution operator
 		void Evol_Site_Construct_(MatrixXcd&);
 
 		// Construct Uz part in time evolution operator
 		void Evol_Z_Construct_(MatrixXcd&);
+
+		void Repr_Init_(); // Initialize the representation string stream as well as type
 
 		const bool debug_; // Used for debug output
 
@@ -57,16 +55,62 @@ class FloEvolRandom : public FloEvolVanilla
 		// Construct evolutionary operator
 		void Evol_Construct(); 
 
-		// Erase the evolutionary operator
-		void Evol_Erase();
-
-		// Return the string format of representation string stream 
-		string Repr() const {return repr_.str();} 
-
-		// Return the type of the model
-		string Type() const {return type_;}
-
 		virtual ~FloEvolRandom() {};
+};
+
+
+//===============================================================================================
+
+
+/*
+ * The random rotation floquet operator. The dimension at each site is 2. Its time 
+ * evolutionary operator is Ux * Uz, where Ux has 2*2 unitary matrix which randomly
+ * rotates any state on a bloch sphere at each site, and Uz is constructed from Ising 
+ * nearest neighbor interactions with interaction strength J.
+ */
+class FloEvolRandomRotation : public FloEvolVanilla
+{
+	struct Param // The parameters used in the model
+	{
+		const double tau; // Time step size
+		const double J; // Nearest neighboring
+
+		Param(double tau, double J): tau(tau), J(J) {};
+	};
+
+	private:
+		const Param param_;
+
+		// The random unit vector for rotation axis
+		vector<vector<double> > axis_;
+
+		// The random angle for rotation about rotation axis
+		vector<double> angle_; 
+
+		// Construct Ux part in time evolution operator
+		void Evol_Site_Construct_(MatrixXcd&);
+
+		// Construct Uz part in time evolution operator
+		void Evol_Z_Construct_(MatrixXcd&);
+
+		void Repr_Init_(); // Initialize the representation string stream as well as type
+
+		const bool debug_; // Used for debug output
+
+	public:
+		FloEvolRandomRotation(int size, double tau, double J):
+			FloEvolVanilla(size), param_(tau, J), debug_(false) { Repr_Init_(); }
+
+		FloEvolRandomRotation(int size, double tau, double J, bool debug):
+			FloEvolVanilla(size), param_(tau, J), debug_(debug) { Repr_Init_(); }
+
+		// Initialize a random angle and a random 3D unit vector
+		void Evol_Para_Init();
+
+		// Construct evolutionary operator
+		void Evol_Construct(); 
+
+		virtual ~FloEvolRandomRotation() {};
 };
 
 #endif
