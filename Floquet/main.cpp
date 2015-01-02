5921c25a2c233987f230273769163c15ab3f2c6a
Liangsheng <liangshe@princeton.edu>
2015-01-02 12:36:40 -0500

    Give angles for random floquet rotation

--- Floquet/main.cpp
+++ Floquet/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <ctime>
+#include "constants.h"
 #include "parameters.h"
 #include "mtrand.h"
 #include "task.h"
@@ -15,7 +16,7 @@ int main(){
 	const bool Level = true;
 
 	parameters.generic.size = 6; // System size
-	parameters.generic.num_realizations = 4; // Number of realizations
+	parameters.generic.num_realizations = 100; // Number of realizations
 	parameters.generic.threads_N = 4; // Number of threads in openmp
 	parameters.generic.evec = false; // Whether compute eigenvectors
 	parameters.generic.erase = true; // Whether erase matrix after diagonization
@@ -27,6 +28,9 @@ int main(){
 	parameters.floquet.J_min = 0; // Minimum J
 	parameters.floquet.J_max = 1; // Maximum J
 	parameters.floquet.tau = 0.8; // Time step size
+
+	parameters.floquet_random.angle_min = Pi; // Minimum angle for the random rotation angle
+	parameters.floquet_random.angle_sup = Pi; // Supreme angle for the random rotation angle
 
 	if (Flo){
 		if (Level){
