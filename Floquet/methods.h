db253a57e8e449a5440420accb77394f8b1c3057
Liangsheng <liangshe@princeton.edu>
2015-01-07 15:39:19 -0500

    Include sigma z construction tpp file in methods.h

--- Floquet/methods.h
+++ Floquet/methods.h
@@ -40,8 +40,9 @@ void level_cal(const AllPara&, vector<EvolMatrix<T1>*>&,
  */
 
 template <class T1, class T2>
-void rightmost_sigma_z_sum(T1&, const vector<T2>&, const string&);
+void rightmost_sigma_z_sum(T1&, const vector< vector<T2> >&, const string&);
 
 #include "level_cal.tpp"
+#include "rightmost_sigma_z_sum.tpp"
 
 #endif
