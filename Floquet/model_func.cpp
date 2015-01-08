c3c5d7e7f17bd4b79ef6926048382eeed0bec29f
Liangsheng <liangshe@princeton.edu>
2015-01-08 14:37:40 -0500

    Implement functions of model_func.h

---
+++ Floquet/model_func.cpp
@@ -0,0 +1,25 @@
+#include "model_func.h"
+#include "flo_evol_model.h"
+
+using namespace std;
+
+void FloEvolRandomFunc::operator() (const AllPara& parameters, 
+EvolMatrix< ComplexEigenSolver<MatrixXcd> >*& model){
+	const int size = parameters.generic.size; // System Size
+	const double tau = parameters.floquet.tau; // Time step, which seems not used here
+	const int J = parameters.floquet.J; // Coupling strength
+
+	model = new FloEvolRandom(size, tau, J);
+}
+
+void FloEvolRandomRotationFunc::operator() (const AllPara& parameters, 
+EvolMatrix< ComplexEigenSolver<MatrixXcd> >*& model){
+	const int size = parameters.generic.size; // System Size
+	const double tau = parameters.floquet.tau; // Time step, which seems not used here
+	const int J = parameters.floquet.J; // Coupling strength
+
+	const double angle_min = parameters.floquet_random.angle_min; // Minimum angle
+	const double angle_sup = parameters.floquet_random.angle_sup; // Supreme angle
+
+	model = new FloEvolRandomRotation(size, tau, J, angle_min, angle_sup);
+}
