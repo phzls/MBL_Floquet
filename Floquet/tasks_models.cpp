10dfc0a30656f2d8ce2c90e2951ad55a8fa08144
Liangsheng <liangshe@princeton.edu>
2015-01-08 15:45:38 -0500

    Use map insert function to construct maps in tasks_models

--- Floquet/tasks_models.cpp
+++ Floquet/tasks_models.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <utility>
+#include <iostream>
 #include "tasks_models.h"
 #include "task_func.h"
 
@@ -7,14 +8,12 @@ using namespace std;
 
 void TasksModels::Map_Construct_(){
 	// Compute level statistics for Floquet system
-	pair<string, task_func> task_name_op1;
 	string task_name1;
 	task_func task_function1;
 
 	task_name1 = "Flo Level";
 	task_function1 = flo_level;
-	task_name_op1 = make_pair(task_name1, task_function1);
-	tasks_.insert(task_name_op1);
+	Task_Map_Insert(task_name1, task_function1);
 
 	// Random Floquet Operator
 	pair<string, ModelFunc*> model_name_op1;
@@ -23,8 +22,7 @@ void TasksModels::Map_Construct_(){
 
 	model_name1 = "Random Flo";
 	model_function1 = new FloEvolRandomFunc();
-	model_name_op1 = make_pair(model_name1, model_function1);
-	models_.insert(model_name_op1);
+	Model_Map_Insert(model_name1, model_function1);
 
 	// Random Rotation Floquet Operator
 	pair<string, ModelFunc*> model_name_op2;
@@ -35,4 +33,28 @@ void TasksModels::Map_Construct_(){
 	model_function2 = new FloEvolRandomRotationFunc();
 	model_name_op2 = make_pair(model_name2, model_function2);
 	models_.insert(model_name_op2);
+}
+
+void TasksModels::Task_Map_Insert(const string& task_name, const task_func& task_function){
+	pair<string, task_func> task_name_op;
+	if (tasks_.find(task_name) == tasks_.end()){ // No duplicate names
+		task_name_op = make_pair(task_name, task_function);
+		tasks_.insert(task_name_op);
+	}
+	else{
+		cout << "Duplicate names for tasks." << endl;
+		abort();
+	}
+}
+
+void TasksModels::Model_Map_Insert(const string& model_name, ModelFunc* const & model_function){
+	pair<string, ModelFunc*> model_name_op;
+	if (models_.find(model_name) == models_.end()){ // No duplicate names
+		model_name_op = make_pair(model_name, model_function);
+		models_.insert(model_name_op);
+	}
+	else{
+		cout << "Duplicate names for models." << endl;
+		abort();
+	}
 }
