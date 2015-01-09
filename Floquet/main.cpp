c6adc9b12ab4851cbe83bd1ddede962b6ab73cae
Liangsheng <liangshe@princeton.edu>
2015-01-09 16:59:39 -0500

    Modify main file for test run

--- Floquet/main.cpp
+++ Floquet/main.cpp
@@ -15,26 +15,27 @@ TasksModels tasks_models; // Record all the tasks and methods
 int main(){
 	AllPara parameters;
 	
-	parameters.generic.task = "Flo Level";
-	parameters.generic.model = "Random Flo";
+	parameters.generic.task = "Flo Rightmost Sigma_z";
+	parameters.generic.model = "Random Rotation Flo";
 
 	parameters.generic.size = 6; // System size
 	parameters.generic.num_realizations = 100; // Number of realizations
-	parameters.generic.threads_N = 4; // Number of threads in openmp
+	parameters.generic.threads_N = 1; // Number of threads in openmp
 	parameters.generic.evec = false; // Whether compute eigenvectors
 	parameters.generic.erase = true; // Whether erase matrix after diagonization
 	parameters.generic.debug = false; // Whether output debug information
 
-	parameters.output.width = 15; // Width for spacing in output files
+	parameters.output.width = 30; // Width for spacing in output files
 	parameters.output.filename_output = true; // Whether print out file names
 
 	parameters.floquet.J_N = 11; // Number of points of coupling strength
 	parameters.floquet.J_min = 0; // Minimum J
 	parameters.floquet.J_max = 1; // Maximum J
 	parameters.floquet.tau = 0.8; // Time step size
+	parameters.floquet.J = 0.9;
 
-	parameters.floquet_random.angle_min = Pi; // Minimum angle for the random rotation angle
-	parameters.floquet_random.angle_sup = Pi; // Supreme angle for the random rotation angle
+	parameters.floquet_random.angle_min = 2*Pi/3; // Minimum angle for the random rotation angle
+	parameters.floquet_random.angle_sup = 2*Pi/3; // Supreme angle for the random rotation angle
 
 	tasks_models.Task(parameters.generic.task)(parameters);
 
