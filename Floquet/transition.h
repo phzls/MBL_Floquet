b24bc67c86ccb5815c23e1ab1c022493e821b716
Liangsheng <liangshe@princeton.edu>
2015-01-12 18:37:09 -0500

    Add an incomplete header file for transition matrices and their functions

---
+++ Floquet/transition.h
@@ -0,0 +1,14 @@
+#ifndef TRANSITION_H
+#define TRANSITION_H
+
+#include <iostream>
+
+using namespace std;
+
+/**
+ ** This file defines the class for basis transition matrix among different bases.
+ **/
+
+class TransitionMatrix{};
+
+#endif
