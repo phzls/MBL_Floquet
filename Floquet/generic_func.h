1cb661435d089c9aa3de0ff464465ab3706786a4
Liangsheng <liangshe@princeton.edu>
2015-01-15 23:57:29 -0500

    Add generic sample mean and standard deviation function in generic_func

--- Floquet/generic_func.h
+++ Floquet/generic_func.h
@@ -29,4 +29,23 @@ T generic_norm(complex<T> val){ return abs(val);}
 
 double generic_norm(double);
 int generic_norm(int);
+
+/*
+ * Given a vector of data, compute its sample mean and the standard deviation from sample variance
+ */
+template<class T>
+void generic_mean_sd(const vector<T> data, T& mean, T& sd){
+	mean = 0;
+	sd = 0;
+
+	const int N = data.size();
+
+	for (int i=0; i<N; i++){
+		mean += data[i];
+		sd += data[i] * data[i];
+	}
+
+	mean /= double(N);
+	sd = sqrt( double(N) / double(N-1) * (sd/double(N) - mean*mean) );
+}
 #endif
