67948a6fde1cb7c161b736a5aaae0938570d83c2
Liangsheng <liangshe@princeton.edu>
2015-01-16 23:47:27 -0500

    Modify main files

--- Floquet/main.cpp
+++ Floquet/main.cpp
@@ -15,24 +15,25 @@ TasksModels tasks_models; // Record all the tasks and methods
 int main(){
 	AllPara parameters;
 	
-	parameters.generic.task = "Flo Evolution";
-	parameters.generic.model = "Random Rotation Flo";
+	parameters.generic.task = "Flo Level";
+	parameters.generic.model = "Inter Random Flo";
 
-	parameters.generic.size = 8; // System size
-	parameters.generic.num_realizations = 400; // Number of realizations
+	parameters.generic.size = 6; // System size
+	parameters.generic.num_realizations = 1; // Number of realizations
 	parameters.generic.threads_N = 4; // Number of threads in openmp
-	parameters.generic.evec = false; // Whether compute eigenvectors
+	parameters.generic.evec = false; // Whether compute eigenvectors, so far only called in
+									 // level statistics calculation
 	parameters.generic.erase = true; // Whether erase matrix after diagonization
 	parameters.generic.debug = false; // Whether output debug information
 
-	parameters.output.width = 15; // Width for spacing in output files
+	parameters.output.width = 30; // Width for spacing in output files
 	parameters.output.filename_output = true; // Whether print out file names
 
 	parameters.floquet.J_N = 11; // Number of points of coupling strength
 	parameters.floquet.J_min = 0; // Minimum J
 	parameters.floquet.J_max = 1; // Maximum J
 	parameters.floquet.tau = 0.8; // Time step size
-	parameters.floquet.J = 0.7;
+	parameters.floquet.J = 1;
 
 	parameters.floquet_random.angle_min = 5*Pi/6; // Minimum angle for the random rotation angle
 	parameters.floquet_random.angle_sup = 5*Pi/6; // Supreme angle for the random rotation angle
@@ -42,7 +43,7 @@ int main(){
 
 	parameters.evolution.time_step = 10; // Number of time steps
 	parameters.evolution.step_size = parameters.floquet.tau; // Time step size
-	parameters.evolution.init_func_name = "Product Random";
+	parameters.evolution.init_func_name = "Random Product";
 	parameters.evolution.evol_compute["Entropy Per Model"] = true;
 	// Initial state name
 	parameters.evolution.model_num = 1; // Number of models for evolution
