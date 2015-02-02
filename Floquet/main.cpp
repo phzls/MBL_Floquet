1c5437e1cd8bddc46d3d7d07f95b69d788a0beb7
Liangsheng <liangshe@princeton.edu>
2015-02-02 15:16:06 -0500

    Change main file for test

--- Floquet/main.cpp
+++ Floquet/main.cpp
@@ -2,24 +2,23 @@
 #include <ctime>
 #include "constants.h"
 #include "parameters.h"
-#include "mtrand.h"
 #include "tasks_models.h"
+#include "randomc.h"
 
 using namespace std;
 
-MTRand u1rand(time(NULL));
-MTRand_closed u2rand(time(NULL));
+CRandomMersenne RanGen_mersenne(time(NULL));
 
 TasksModels tasks_models; // Record all the tasks and methods
 
 int main(){
 	AllPara parameters;
 	
-	parameters.generic.task = "Flo Rightmost Sigma_z";
-	parameters.generic.model = "Inter Random Flo";
+	parameters.generic.task = "Flo Simple Markov Evolution";
+	parameters.generic.model = "Markov Inter Random Flo";
 
-	parameters.generic.size = 10; // System size
-	parameters.generic.num_realizations = 1; // Number of realizations
+	parameters.generic.size = 8; // System size
+	parameters.generic.num_realizations = 100; // Number of realizations
 	parameters.generic.threads_N = 4; // Number of threads in openmp
 	parameters.generic.evec = false; // Whether compute eigenvectors, so far only called in
 									 // level statistics calculation
@@ -33,7 +32,7 @@ int main(){
 	parameters.floquet.J_min = 0; // Minimum J
 	parameters.floquet.J_max = 1; // Maximum J
 	parameters.floquet.tau = 0.8; // Time step size
-	parameters.floquet.J = 0.9;
+	parameters.floquet.J = 0.4;
 
 	parameters.floquet_random.angle_min = 5*Pi/6; // Minimum angle for the random rotation angle
 	parameters.floquet_random.angle_sup = 5*Pi/6; // Supreme angle for the random rotation angle
@@ -51,7 +50,7 @@ int main(){
 	parameters.evolution.left_size = parameters.generic.size / 2; 
 	parameters.evolution.jump = 1; // jump of time points in evolution
 
-	parameters.evolution.log_time = true; // whehter time changes logarithmically
+	parameters.evolution.log_time = false; // whehter time changes logarithmically
 	parameters.evolution.log_time_jump = 2; // The base for time change logarithmically
 
 
