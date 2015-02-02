319ec1921b7e3c60cb72cc098918ad7f0f42993a
Liangsheng <liangshe@princeton.edu>
2015-02-02 15:14:49 -0500

    Change random number generating in random_pure_amplitude

--- Floquet/random_pure_amplitude.cpp
+++ Floquet/random_pure_amplitude.cpp
@@ -1,26 +1,24 @@
 #include <complex>
 #include <vector>
 #include "constants.h"
-#include "mtrand.h"
-
-using namespace std;
+#include "randomc.h"
 
 /**
  ** This function creates a vector of random complex amplitudes which can be used for random
  ** pure state
  **/
 
-extern MTRand u1rand;
+extern CRandomMersenne RanGen_mersenne; // points in [0,1)
 
 void random_pure_amplitude(vector<complex<double> >& amplitude){
 	double sum = 0;
 	for (int i=0;i<amplitude.size();i++)
 	{
-		double U1 = u1rand();
-		double U2 = u1rand();
+		double U1 = RanGen_mersenne.Random();
+		double U2 = RanGen_mersenne.Random();
 
-		double real = sqrt(-2*log(U1))*cos(2*Pi*U2);
-		double imag = sqrt(-2*log(U1))*sin(2*Pi*U2);
+		double real = sqrt(-2*log(1-U1))*cos(2*Pi*U2);
+		double imag = sqrt(-2*log(1-U1))*sin(2*Pi*U2);
 
 		amplitude[i] = complex<double>(real,imag);
 
