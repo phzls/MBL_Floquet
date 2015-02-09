6cf2290400c5a8e304efd837901cb2fc70fac217
Liangsheng <liangshe@princeton.edu>
2015-02-09 16:28:17 -0500

    Add markov_left_spin

---
+++ Floquet/Matlab Test/markov_left_spin.m
@@ -0,0 +1,41 @@
+function left_spin = markov_left_spin(L,T,J,Ur, state, debug)
+
+tau = 0.8;
+[U_up, H] = XXZ_bath_binary(L, J*tau, 1);
+[U_down, H] = XXZ_bath_binary(L, J*tau, -1);
+
+U_up = Ur * U_up;
+U_down = Ur * U_down;
+
+% Given initial state, compute density matrix
+density = state * state';
+
+if debug
+    disp(0);
+    disp(density);
+end
+
+% Left spin
+left_spin = [];
+
+% Time evolution
+for t=0:T-1
+    d = diag(density);
+    spin = mod( idivide(int32(i), int32(2^(L-1))), 2 )
+    left = 0
+    for i=1:length(d)
+        if (spin == 0)
+            left = left - d[i];
+        else
+            left = left + d[i];
+        end
+    left_spin = [left_spin, left];
+    temp = U_up * density * U_up' + U_down * density * U_down';
+    density = 0.5*temp;
+    if debug
+        disp(t+1);
+        disp(density);
+    end
+end
+
+
