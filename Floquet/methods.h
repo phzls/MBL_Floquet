e1808621c9e2e16cbdbeafe66adcb55409e0e487
Liangsheng <liangshe@princeton.edu>
2015-02-10 17:39:16 -0500

    Add leftmost_sigma_z in methods

--- Floquet/methods.h
+++ Floquet/methods.h
@@ -59,8 +59,25 @@ void evec_to_basic(EvolMatrix<T1>* const &, vector<vector<T2> >&);
  */
 void reduced_density_left_2(const VectorXcd&, int, int, MatrixXcd&);
 
+/*
+ * Construct the matrix representation of a sigma z operator at leftmost site of the chain.
+ * T1 gives the type of the matrix represation. T2 is the type of basis vectors which the
+ * representation is about to be written in. The basis vectors must be written in binary basis,
+ * i.e., the basis of eigenstates of sigma_z matrix at each site, and with spin down = 0, 
+ * spin_up = 1, and the amplitudes are listed in ascending order of the corresponding binary
+ * numbers. The string can take values as: "entry" and "norm", which specifies what the matrix
+ * store, i.e., the exact entry or the norm. If the T1 object passed in has dimension 0, then 
+ * it will be resized to the appropriate size, otherwise a check of size will be performed, and
+ * new values will be added to the existing values.
+ */
+
+template <class T1, class T2>
+void leftmost_sigma_z_sum(Matrix<T1, Dynamic, Dynamic>&, const vector< vector<T2> >&, 
+	const string&);
+
 #include "level_cal.tpp"
 #include "rightmost_sigma_z_sum.tpp"
 #include "evec_to_basic.tpp"
+#include "leftmost_sigma_z_sum.tpp"
 
 #endif
