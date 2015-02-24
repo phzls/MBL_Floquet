64c8a48a78dbd0d32bce4dede27f87546bf4d7d4
Liangsheng <liangshe@princeton.edu>
2015-02-23 21:18:29 -0500

    Change file name generating script

--- Floquet/Markov Flo Evol/file_gen.py
+++ Floquet/Markov Flo Evol/file_gen.py
@@ -9,11 +9,11 @@ suffix = ".txt"
 
 # Read all files starting with Markov
 for file in os.listdir('.'):
-    if fnmatch.fnmatch(file, "Markov*spin*.txt"):
-        print file[:-len(suffix)]
+    if fnmatch.fnmatch(file, "Markov*J=0.9*spin*.txt"):
+#        print file[:-len(suffix)]
         markov_file.append(file[:-len(suffix)])
 
-f = open("spin_name.txt",'w')
+f = open("spin_thermal_name.txt",'w')
 for n in markov_file:
     print >> f, n
 f.close()
