fbad2c42a65172bebd2a176c66022129900a7689
Liangsheng <liangshe@princeton.edu>
2015-03-24 16:51:33 -0400

    Add some comments in main

--- Floquet/main.cpp
+++ Floquet/main.cpp
@@ -127,8 +127,9 @@ int main(){
 
 
 
-	// Methods to be called under single model
-	parameters.transition.flo_transition_compute["ZZ Correlation Square"] = true;
+	// Methods to be called for studying transition of floquet systems from thermal to localization
+	parameters.transition.flo_transition_compute["ZZ Correlation Square"] = true; // End-to-end sigma_z X sigma_z
+																				  // correlation square
 
 
 
