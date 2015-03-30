01550b2e65d406cb5f022831456530bbb14e8bb1
Liangsheng <liangshe@princeton.edu>
2015-03-30 18:25:11 -0400

    Add python script to generating files in flo_transition

---
+++ Floquet/Floquet Transition/file_gen.py
@@ -0,0 +1,21 @@
+__author__ = 'liangshengzhang'
+
+import os
+import fnmatch
+
+# Markov file names
+markov_file = []
+
+# This is not included in the file name
+suffix = ".txt"
+
+# Read all files starting with Markov
+for file in os.listdir('.'):
+    if fnmatch.fnmatch(file, "*.txt"):
+#        print file[:-len(suffix)]
+        markov_file.append(file[:-len(suffix)])
+
+f = open("name.txt",'w')
+for n in markov_file:
+    print >> f, n
+f.close()
