160f25e030e58561d13aa00dca02756e3fc203d3
Liangsheng <liangshe@princeton.edu>
2015-03-30 18:18:54 -0400

    Add flo_evol_xxz_random_simp_shift in flo_evol_model

--- Floquet/flo_evol_model.h
+++ Floquet/flo_evol_model.h
@@ -700,4 +700,51 @@ public:
 
 
 
+
+
+
+//=============================================================================================
+
+
+
+
+
+
+
+/*
+ * This operator constructs the shifted version of flo_evol_xxz_random_simp which is a simplified version of
+ * flo_evol_xxz_random with many parameters equal to 1. Effectively, it is sqrt(U_x) * U_z * sqrt(U_x)
+ */
+class FloEvolXXZRandomSimpShift : public FloEvolVanilla
+{
+private:
+	const double W_; // Disorder strength
+
+	// Construct x part of time evolution operator
+	void Evol_X_Construct_(MatrixXcd&);
+
+	// Construct z part of time evolution operator
+	void Evol_Z_Construct_(MatrixXcd&);
+
+	void Repr_Init_(); // Initialize the representation string stream as well as type
+
+	const bool debug_; // Used for debug outputs
+
+	vector<double> random_h_; // Random longitude field part at each site
+
+public:
+	FloEvolXXZRandomSimpShift(int size, double W, bool debug = false):
+			FloEvolVanilla(size), W_(W), debug_(debug) { Repr_Init_();}
+
+	// Construct evolutionary operator
+	void Evol_Construct();
+
+	// No parameter to initialize
+	void Evol_Para_Init();
+
+	virtual ~FloEvolXXZRandomSimpShift() {};
+};
+
+
+
 #endif
