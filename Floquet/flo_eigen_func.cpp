98b09a5fa2e9c2c4722f51721078ab50330bd15e
Liangsheng <liangshe@princeton.edu>
2015-04-03 18:36:33 -0400

    Implement flo_eigen_func which contains classes for flo_eigen

---
+++ Floquet/flo_eigen_func.cpp
@@ -0,0 +1,198 @@
+//
+// Created by Liangsheng Zhang on 4/3/15.
+//
+
+#include <iostream>
+#include <cstdlib>
+#include "flo_eigen_func.h"
+
+using namespace std;
+
+void FloEigenFunc::map_initialize_(const AllPara& parameters) {
+
+    map<string, Flo_init >::iterator init_it;
+    map<string, Flo_func >::iterator cal_it;
+    map<string, Flo_out >::iterator out_it;
+
+    // end-to-end z-z correlation square
+    string name1 = "ZZ Correlation Square";
+    Flo_init init_func1 = &FloEigenFunc::ZZ_corr_square_init_;
+    Flo_func cal_func1 = &FloEigenFunc::ZZ_corr_square_compute_;
+    Flo_out out_func1 = &FloEigenFunc::ZZ_corr_square_out_;
+
+    // Make sure the name has not been used before
+    init_it = flo_init_map_.find(name1);
+    cal_it = flo_func_map_.find(name1);
+    out_it = flo_out_map_.find(name1);
+    if (init_it != flo_init_map_.end() || cal_it != flo_func_map_.end() || out_it != flo_out_map_.end()){
+        cout << name1 << " for floquet transition has appeared before." << endl;
+        abort();
+    }
+
+    flo_init_map_[name1] = init_func1;
+    flo_func_map_[name1] = cal_func1;
+    flo_out_map_[name1] = out_func1;
+
+    // entropy variance
+    string name2 = "Entropy Variance";
+    Flo_init init_func2 = &FloEigenFunc::Ent_var_init_;
+    Flo_func cal_func2 = &FloEigenFunc::Ent_var_compute_;
+    Flo_out out_func2 = &FloEigenFunc::Ent_var_out_;
+
+    // Make sure the name has not been used before
+    init_it = flo_init_map_.find(name2);
+    cal_it = flo_func_map_.find(name2);
+    out_it = flo_out_map_.find(name2);
+    if (init_it != flo_init_map_.end() || cal_it != flo_func_map_.end() || out_it != flo_out_map_.end()){
+        cout << name2 << " for floquet transition has appeared before." << endl;
+        abort();
+    }
+
+    flo_init_map_[name2] = init_func2;
+    flo_func_map_[name2] = cal_func2;
+    flo_out_map_[name2] = out_func2;
+
+    // entropy variance for eigenstate with smallest phase magnitude among all realizations
+    string name3 = "Entropy Variance Smallest";
+    Flo_init init_func3 = &FloEigenFunc::Ent_smallest_var_init_;
+    Flo_func cal_func3 = &FloEigenFunc::Ent_smallest_var_compute_;
+    Flo_out out_func3 = &FloEigenFunc::Ent_smallest_var_out_;
+
+    // Make sure the name has not been used before
+    init_it = flo_init_map_.find(name3);
+    cal_it = flo_func_map_.find(name3);
+    out_it = flo_out_map_.find(name3);
+    if (init_it != flo_init_map_.end() || cal_it != flo_func_map_.end() || out_it != flo_out_map_.end()){
+        cout << name3 << " for floquet transition has appeared before." << endl;
+        abort();
+    }
+
+    flo_init_map_[name3] = init_func3;
+    flo_func_map_[name3] = cal_func3;
+    flo_out_map_[name3] = out_func3;
+
+    // end-to-end z-z time correlation square
+    string name4 = "ZZ Time Correlation";
+    Flo_init init_func4 = &FloEigenFunc::ZZ_time_corr_init_;
+    Flo_func cal_func4 = &FloEigenFunc::ZZ_time_corr_compute_;
+    Flo_out out_func4 = &FloEigenFunc::ZZ_time_corr_out_;
+
+    // Make sure the name has not been used before
+    init_it = flo_init_map_.find(name4);
+    cal_it = flo_func_map_.find(name4);
+    out_it = flo_out_map_.find(name4);
+    if (init_it != flo_init_map_.end() || cal_it != flo_func_map_.end() || out_it != flo_out_map_.end()){
+        cout << name4 << " for floquet transition has appeared before." << endl;
+        abort();
+    }
+
+    flo_init_map_[name4] = init_func4;
+    flo_func_map_[name4] = cal_func4;
+    flo_out_map_[name4] = out_func4;
+
+    // end-to-end z-z time correlation square
+    string name5 = "ZZ Time Correlation Components";
+    Flo_init init_func5 = &FloEigenFunc::ZZ_time_corr_component_init_;
+    Flo_func cal_func5 = &FloEigenFunc::ZZ_time_corr_component_compute_;
+    Flo_out out_func5 = &FloEigenFunc::ZZ_time_corr_component_out_;
+
+    // Make sure the name has not been used before
+    init_it = flo_init_map_.find(name5);
+    cal_it = flo_func_map_.find(name5);
+    out_it = flo_out_map_.find(name5);
+    if (init_it != flo_init_map_.end() || cal_it != flo_func_map_.end() || out_it != flo_out_map_.end()){
+        cout << name5 << " for floquet transition has appeared before." << endl;
+        abort();
+    }
+
+    flo_init_map_[name5] = init_func5;
+    flo_func_map_[name5] = cal_func5;
+    flo_out_map_[name5] = out_func5;
+
+    // Check the number of function
+    if ( flo_init_map_.size() != flo_func_map_.size() ){
+
+        cout << "Number of initializing functions in flo_transition is not the same as number of registered functions"
+        << "that can be called." << endl;
+        cout << "Functions in parameters:" << endl;
+        for (map<string, Flo_init >::iterator it = flo_init_map_.begin();
+             it != flo_init_map_.end(); it++){
+            cout << it -> first << endl;
+        }
+        cout << "Total Number: " << flo_init_map_.size() << endl;
+
+        cout << "Functions that can be called:" << endl;
+        for (map<string, Flo_func>::iterator it = flo_func_map_.begin();
+             it != flo_func_map_.end(); it ++){
+            cout << it -> first << endl;
+        }
+        cout << "Total Number:" << flo_func_map_.size() << endl;
+
+        abort();
+    }
+
+    if ( flo_out_map_.size() != flo_func_map_.size() ){
+
+        cout << "Number of output functions in flo_transition is not the same as number of registered functions"
+        << "that can be called." << endl;
+        cout << "Functions in parameters:" << endl;
+        for (map<string, Flo_out>::iterator it = flo_out_map_.begin();
+             it != flo_out_map_.end(); it++){
+            cout << it -> first << endl;
+        }
+        cout << "Total Number: " << flo_out_map_.size() << endl;
+
+        cout << "Functions that can be called:" << endl;
+        for (map<string, Flo_func>::iterator it = flo_func_map_.begin();
+             it != flo_func_map_.end(); it ++){
+            cout << it -> first << endl;
+        }
+        cout << "Total Number:" << flo_func_map_.size() << endl;
+
+        abort();
+    }
+
+    if ( flo_func_bool_map_.size() > flo_func_map_.size() ){
+
+        cout << "Number of registered functions in flo_transition is larger than the number of registered functions"
+        << "that can be called." << endl;
+        cout << "Functions in parameters:" << endl;
+        for (map<string, bool>::iterator it = flo_func_bool_map_.begin();
+             it != flo_func_bool_map_.end(); it++){
+            cout << it -> first << endl;
+        }
+        cout << "Total Number: " << flo_func_bool_map_.size() << endl;
+
+        cout << "Functions that can be calledt:" << endl;
+        for (map<string, Flo_func>::iterator it = flo_func_map_.begin();
+             it != flo_func_map_.end(); it ++){
+            cout << it -> first << endl;
+        }
+        cout << "Total Number:" << flo_func_map_.size() << endl;
+
+        abort();
+    }
+
+    // Initialize data structures
+    for (map<string, bool>::iterator it = flo_func_bool_map_.begin(); it != flo_func_bool_map_.end(); it++){
+        if (it -> second) ( this ->*(flo_init_map_[it -> first]) ) (parameters);
+    }
+}
+
+void FloEigenFunc::Compute(AllPara const & parameters,
+                                 EvolMatrix <ComplexEigenSolver<MatrixXcd> > const * floquet,
+                                 LocalInfo const & local_info) {
+
+    for (map<string, bool>::iterator it = flo_func_bool_map_.begin(); it != flo_func_bool_map_.end(); it++){
+        if (it -> second) ( this ->*(flo_func_map_[it -> first]) )(parameters, floquet, local_info);
+    }
+}
+
+void FloEigenFunc::Output(AllPara const & parameters, const string& name) {
+    for (map<string, bool>::iterator it = flo_func_bool_map_.begin(); it != flo_func_bool_map_.end(); it++){
+        if (it -> second) ( this ->*(flo_out_map_[it -> first]) )(parameters, name);
+    }
+}
+
+
+
---
+++ Floquet/flo_eigen_func.h
@@ -0,0 +1,106 @@
+//
+// Created by Liangsheng Zhang on 4/3/15.
+//
+
+#ifndef FLOQUET_FLO_EIGEN_FUNC_H
+#define FLOQUET_FLO_EIGEN_FUNC_H
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include "parameters.h"
+#include "evol_class.h"
+
+using namespace std;
+
+/**
+ ** This file contains relevant classes for flo_eigen
+ **/
+
+/*
+ * The data class used in flo_eigen. It contains all possible outputs.
+ */
+
+struct EigenData
+{
+    // End-to-end sigma_z-sigma_z correlation square for all eigenstates in all realizations
+    vector< vector<double> > zz_corr_square;
+
+    // Entropy varaince for all eigenstates
+    vector< vector<double> > ent_var;
+
+    // Entropy variance for the eigenstate with smallest phase magnitude among different realizations
+    vector< vector<double> > ent_smallest_var;
+
+    // End-to-end sigma_z-sigma_z time four-point correlation
+    vector< vector<double> > zz_time_corr;
+
+    // Various components of zz time correlation
+    vector< vector<vector<double> > > zz_time_corr_component;
+
+};
+
+/*
+ * Local information that helps computation
+ */
+struct LocalInfo
+{
+    int J_index; // The index of J
+    int realization_index; // The index of realization
+};
+
+class FloEigenFunc;
+
+// Pointer to functions initializing data
+typedef  void (FloEigenFunc::*Flo_init)(const AllPara&);
+
+// Pointer to functions studying properties of a given floquet system
+typedef void (FloEigenFunc::*Flo_func)(const AllPara&, const EvolMatrix<ComplexEigenSolver<MatrixXcd> >*,
+                                             const LocalInfo&);
+
+// Pointer to functions outputting data. The string gives the first part of filename
+typedef void (FloEigenFunc::*Flo_out)(const AllPara&, const string&);
+
+/*
+ * The class that contains data and functions which handle it
+ */
+class FloEigenFunc
+{
+private:
+    // Map that determines which functions are computed
+    map<string, bool> flo_func_bool_map_;
+
+    // Map that links strings to initialization functions
+    map<string, Flo_init> flo_init_map_;
+
+    // Map that links strings to computation functions
+    map<string, Flo_func> flo_func_map_;
+
+    // Map that links strings to output functions
+    map<string, Flo_out> flo_out_map_;
+
+    // Initialize all maps
+    void map_initialize_(const AllPara&);
+
+    EigenData model_data_;
+
+    // For end to end zz correlation square statistics
+    void ZZ_corr_square_init_(const AllPara&);
+    void ZZ_corr_square_compute_(const AllPara&, const EvolMatrix<ComplexEigenSolver<MatrixXcd> >*, const LocalInfo&);
+    void ZZ_corr_square_out_(const AllPara&, const string&);
+
+
+
+public:
+    FloEigenFunc(const AllPara& parameters) : flo_func_bool_map_(parameters.transition.flo_transition_compute) {
+        map_initialize_(parameters);
+    }
+
+    void Compute(const AllPara&, const EvolMatrix<ComplexEigenSolver<MatrixXcd> >*, const LocalInfo&);
+    void Output(const AllPara&, const string&);
+
+    ~FloEigenFunc() {};
+};
+
+
+#endif //FLOQUET_FLO_EIGEN_FUNC_H
