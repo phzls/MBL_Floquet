0502b3131eb364f9efdafff3fbdc9e3d0f2789ff
Liangsheng <liangshe@princeton.edu>
2015-04-06 15:14:44 -0400

    Delete not-yet-implemented functions in flo_eigen_func

--- Floquet/flo_eigen_func.cpp
+++ Floquet/flo_eigen_func.cpp
@@ -14,11 +14,11 @@ void FloEigenFunc::map_initialize_(const AllPara& parameters) {
     map<string, Flo_func >::iterator cal_it;
     map<string, Flo_out >::iterator out_it;
 
-    // end-to-end z-z correlation square
+    // end-to-end z-z correlation square for all eigenstates
     string name1 = "ZZ Correlation Square";
-    Flo_init init_func1 = &FloEigenFunc::ZZ_corr_square_init_;
-    Flo_func cal_func1 = &FloEigenFunc::ZZ_corr_square_compute_;
-    Flo_out out_func1 = &FloEigenFunc::ZZ_corr_square_out_;
+    Flo_init init_func1 = &FloEigenFunc::ZZ_corr_square_eigen_init_;
+    Flo_func cal_func1 = &FloEigenFunc::ZZ_corr_square_eigen_compute_;
+    Flo_out out_func1 = &FloEigenFunc::ZZ_corr_square_eigen_out_;
 
     // Make sure the name has not been used before
     init_it = flo_init_map_.find(name1);
@@ -32,82 +32,6 @@ void FloEigenFunc::map_initialize_(const AllPara& parameters) {
     flo_init_map_[name1] = init_func1;
     flo_func_map_[name1] = cal_func1;
     flo_out_map_[name1] = out_func1;
-
-    // entropy variance
-    string name2 = "Entropy Variance";
-    Flo_init init_func2 = &FloEigenFunc::Ent_var_init_;
-    Flo_func cal_func2 = &FloEigenFunc::Ent_var_compute_;
-    Flo_out out_func2 = &FloEigenFunc::Ent_var_out_;
-
-    // Make sure the name has not been used before
-    init_it = flo_init_map_.find(name2);
-    cal_it = flo_func_map_.find(name2);
-    out_it = flo_out_map_.find(name2);
-    if (init_it != flo_init_map_.end() || cal_it != flo_func_map_.end() || out_it != flo_out_map_.end()){
-        cout << name2 << " for floquet transition has appeared before." << endl;
-        abort();
-    }
-
-    flo_init_map_[name2] = init_func2;
-    flo_func_map_[name2] = cal_func2;
-    flo_out_map_[name2] = out_func2;
-
-    // entropy variance for eigenstate with smallest phase magnitude among all realizations
-    string name3 = "Entropy Variance Smallest";
-    Flo_init init_func3 = &FloEigenFunc::Ent_smallest_var_init_;
-    Flo_func cal_func3 = &FloEigenFunc::Ent_smallest_var_compute_;
-    Flo_out out_func3 = &FloEigenFunc::Ent_smallest_var_out_;
-
-    // Make sure the name has not been used before
-    init_it = flo_init_map_.find(name3);
-    cal_it = flo_func_map_.find(name3);
-    out_it = flo_out_map_.find(name3);
-    if (init_it != flo_init_map_.end() || cal_it != flo_func_map_.end() || out_it != flo_out_map_.end()){
-        cout << name3 << " for floquet transition has appeared before." << endl;
-        abort();
-    }
-
-    flo_init_map_[name3] = init_func3;
-    flo_func_map_[name3] = cal_func3;
-    flo_out_map_[name3] = out_func3;
-
-    // end-to-end z-z time correlation square
-    string name4 = "ZZ Time Correlation";
-    Flo_init init_func4 = &FloEigenFunc::ZZ_time_corr_init_;
-    Flo_func cal_func4 = &FloEigenFunc::ZZ_time_corr_compute_;
-    Flo_out out_func4 = &FloEigenFunc::ZZ_time_corr_out_;
-
-    // Make sure the name has not been used before
-    init_it = flo_init_map_.find(name4);
-    cal_it = flo_func_map_.find(name4);
-    out_it = flo_out_map_.find(name4);
-    if (init_it != flo_init_map_.end() || cal_it != flo_func_map_.end() || out_it != flo_out_map_.end()){
-        cout << name4 << " for floquet transition has appeared before." << endl;
-        abort();
-    }
-
-    flo_init_map_[name4] = init_func4;
-    flo_func_map_[name4] = cal_func4;
-    flo_out_map_[name4] = out_func4;
-
-    // end-to-end z-z time correlation square
-    string name5 = "ZZ Time Correlation Components";
-    Flo_init init_func5 = &FloEigenFunc::ZZ_time_corr_component_init_;
-    Flo_func cal_func5 = &FloEigenFunc::ZZ_time_corr_component_compute_;
-    Flo_out out_func5 = &FloEigenFunc::ZZ_time_corr_component_out_;
-
-    // Make sure the name has not been used before
-    init_it = flo_init_map_.find(name5);
-    cal_it = flo_func_map_.find(name5);
-    out_it = flo_out_map_.find(name5);
-    if (init_it != flo_init_map_.end() || cal_it != flo_func_map_.end() || out_it != flo_out_map_.end()){
-        cout << name5 << " for floquet transition has appeared before." << endl;
-        abort();
-    }
-
-    flo_init_map_[name5] = init_func5;
-    flo_func_map_[name5] = cal_func5;
-    flo_out_map_[name5] = out_func5;
 
     // Check the number of function
     if ( flo_init_map_.size() != flo_func_map_.size() ){
