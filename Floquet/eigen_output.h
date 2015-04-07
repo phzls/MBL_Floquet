b62cefae045fd7d086e5501a7534a4c189533145
Liangsheng <liangshe@princeton.edu>
2015-04-07 11:05:06 -0400

    Add a function for processing complex number into a string in eigen_output

--- Floquet/eigen_output.h
+++ Floquet/eigen_output.h
@@ -48,6 +48,39 @@ void complex_write(const complex<T>& val){
 }
 
 /*
+ * Return a complex number in string. If the imaginary part has magnitude smaller than delta, then
+ * it is assumed to 0 and not written out. If the imaginary part has magnitude smaller than
+ * delta, then it is assumed to 0 and not written out. If both real and imaginary parts are
+ * not written out, then a 0 is outputted.
+ */
+template <class T>
+string complex_write_return(const complex<T>& val){
+	stringstream c_num;
+	const double delta = 1.0e-15;
+
+	T re = real(val);
+	bool re_out = true;
+
+	T im = imag(val);
+	bool im_out = true;
+
+	if (abs(re) > delta){
+		c_num << real(val);
+	}
+	else re_out = false;
+
+	if(abs(im)>delta){
+		if (im>=0 && re_out) c_num << "+";
+		c_num << im << "j";
+	}
+	else im_out = false;
+
+	if(!re_out && !im_out) c_num << 0;
+
+	return c_num.str();
+}
+
+/*
  * Use complex_write function to write out an Eigen dynamic complex matrix
  */
 void complex_matrix_write(const MatrixXcd&);
